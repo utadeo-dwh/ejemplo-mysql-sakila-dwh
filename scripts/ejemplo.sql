8edf89bfbc543a1865237ccf2c84f33336980ca8
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2023-09-30 14:52:24 +0000

    Modifica descripción en ejemplo.sql

--- scripts/ejemplo.sql
+++ scripts/ejemplo.sql
@@ -1,7 +1,7 @@
 # Script de ejemplo
 # -----------------
 # Para ejecutarlo en el MySQL en el contenedor se puede ejecutar
-#   source /scripts/ejemplo.MySQL
+#   source /scripts/ejemplo.sql
 
 use sakila;
 
